4fa07cc1da0e8a1bb2d573556d1e930065bffc12
Kunmeer-SyedMohamedHyder <syed-mohamed-hyder.kunmeer@hpe.com>
2023-05-27 19:59:49 +0530

    Minimal run to play with public and private ecdsa keys.

--- app/scratch/main.go
+++ app/scratch/main.go
@@ -31,7 +31,7 @@ func run() error {
 		Value:  1000,
 	}
 
-	privateKey, err := crypto.LoadECDSA("C:\\Users\\kunmeer\\go\\src\\github.com\\blockchain\\zblock\\accounts\\baba.ecdsa")
+	privateKey, err := crypto.LoadECDSA("zblock/accounts/kennedy.ecdsa")
 	if err != nil {
 		return fmt.Errorf("unable to load private key: %w", err)
 	}
@@ -41,13 +41,26 @@ func run() error {
 		return fmt.Errorf("unable to marshal: %w", err)
 	}
 
+	v := crypto.Keccak256(data)
+
 	// Sign the hash with the private key to produce a signature.
-	sig, err := crypto.Sign(data, privateKey)
+	sig, err := crypto.Sign(v, privateKey)
 	if err != nil {
-		return err
+		return fmt.Errorf("unable to sign: %w", err)
 	}
 
-	fmt.Println(hexutil.Encode(sig))
+	fmt.Println("SIG:", hexutil.Encode(sig))
+
+	//=======================================================================================
+	// DOWN THE WIRE
+
+	// see if the v has changed, the public key received from the signature will also change.
+	publicKey, err := crypto.SigToPub(v, sig)
+	if err != nil {
+		return fmt.Errorf("unable to pub: %w", err)
+	}
+
+	fmt.Println("PUB:", crypto.PubkeyToAddress(*publicKey).String())
 
 	return nil
 }
