f67a8f3167f19410ed8d6c6580d420059a459d2a
Kunmeer-SyedMohamedHyder <syed-mohamed-hyder.kunmeer@hpe.com>
2023-06-03 12:56:48 +0530

    Database support and account types added.

--- foundation/blockchain/database/account.go
+++ foundation/blockchain/database/account.go
@@ -1,6 +1,31 @@
 package database
 
-import "errors"
+import (
+	"crypto/ecdsa"
+	"errors"
+
+	"github.com/ethereum/go-ethereum/crypto"
+)
+
+// Account represents information stored in the database for an individual account.
+type Account struct {
+	AccountID AccountID
+	Nonce     uint64
+	Balance   uint64
+}
+
+// newAccount constructs a new account value for use.
+func newAccount(accountID AccountID, balance uint64) Account {
+	return Account{
+		AccountID: accountID,
+		Balance:   balance,
+	}
+}
+
+// PublicKeyToAccountID converts the public key to an account value.
+func PublicKeyToAccountID(pk ecdsa.PublicKey) AccountID {
+	return AccountID(crypto.PubkeyToAddress(pk).String())
+}
 
 // =============================================================================
 
