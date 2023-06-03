b74ce5c59484e0137e55ab966cff70eb6e928ece
Kunmeer-SyedMohamedHyder <syed-mohamed-hyder.kunmeer@hpe.com>
2023-06-03 13:00:02 +0530

    App startup with state initialization.

--- app/services/node/main.go
+++ app/services/node/main.go
@@ -11,8 +11,12 @@ import (
 	"time"
 
 	"github.com/Kunmeer-SyedMohamedHyder/blockchain/app/services/node/handlers"
+	"github.com/Kunmeer-SyedMohamedHyder/blockchain/foundation/blockchain/database"
+	"github.com/Kunmeer-SyedMohamedHyder/blockchain/foundation/blockchain/genesis"
+	"github.com/Kunmeer-SyedMohamedHyder/blockchain/foundation/blockchain/state"
 	"github.com/Kunmeer-SyedMohamedHyder/blockchain/foundation/logger"
 	"github.com/ardanlabs/conf/v3"
+	"github.com/ethereum/go-ethereum/crypto"
 	"go.uber.org/zap"
 )
 
@@ -56,6 +60,16 @@ func run(log *zap.SugaredLogger) error {
 			PublicHost      string        `conf:"default:0.0.0.0:8080"`
 			PrivateHost     string        `conf:"default:0.0.0.0:9080"`
 		}
+		State struct {
+			Beneficiary    string   `conf:"default:miner1"`
+			DBPath         string   `conf:"default:zblock/miner1/"`
+			SelectStrategy string   `conf:"default:Tip"`
+			OriginPeers    []string `conf:"default:0.0.0.0:9080"` //
+			Consensus      string   `conf:"default:POW"`          // Change to POA to run Proof of Authority
+		}
+		NameService struct {
+			Folder string `conf:"default:zblock/accounts/"`
+		}
 	}{
 		Version: conf.Version{
 			Build: build,
@@ -94,6 +108,43 @@ func run(log *zap.SugaredLogger) error {
 		return fmt.Errorf("generating config for output: %w", err)
 	}
 	log.Infow("startup", "config", out)
+
+	// =========================================================================
+	// Blockchain Support
+
+	// Need to load the private key file for the configured beneficiary so the
+	// account can get credited with fees and tips.
+	path := fmt.Sprintf("%s%s.ecdsa", cfg.NameService.Folder, cfg.State.Beneficiary)
+	privateKey, err := crypto.LoadECDSA(path)
+	if err != nil {
+		return fmt.Errorf("unable to load private key for node: %w", err)
+	}
+
+	ev := func(v string, args ...any) {
+
+		s := fmt.Sprintf(v, args...)
+		log.Infow(s, "traceid", "00000000-0000-0000-0000-000000000000")
+
+	}
+
+	// Load the genesis file for blockchain settings and origin balances.
+	genesis, err := genesis.Load()
+	if err != nil {
+		return err
+	}
+
+	// The state value represents the blockchain node and manages the blockchain
+	// database and provides an API for application support.
+	state, err := state.New(state.Config{
+		BeneficiaryID: database.PublicKeyToAccountID(privateKey.PublicKey),
+		Genesis:       genesis,
+
+		EvHandler: ev,
+	})
+	if err != nil {
+		return err
+	}
+	defer state.Shutdown()
 
 	// =========================================================================
 	// Start Debug Service
