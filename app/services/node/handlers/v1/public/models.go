29169bc76037f69fe6eb59c0d33c1ba6d9b1da36
Kunmeer-SyedMohamedHyder <syed-mohamed-hyder.kunmeer@hpe.com>
2023-06-06 14:40:10 +0530

    New API routes and handlers added.

---
+++ app/services/node/handlers/v1/public/models.go
@@ -0,0 +1,28 @@
+package public
+
+import "github.com/Kunmeer-SyedMohamedHyder/blockchain/foundation/blockchain/database"
+
+type act struct {
+	Account database.AccountID `json:"account"`
+	Name    string             `json:"name"`
+	Balance uint64             `json:"balance"`
+	Nonce   uint64             `json:"nonce"`
+}
+
+type tx struct {
+	FromAccount database.AccountID `json:"from"`
+	FromName    string             `json:"from_name"`
+	To          database.AccountID `json:"to"`
+	ToName      string             `json:"to_name"`
+	ChainID     uint16             `json:"chain_id"`
+	Nonce       uint64             `json:"nonce"`
+	Value       uint64             `json:"value"`
+	Tip         uint64             `json:"tip"`
+	Data        []byte             `json:"data"`
+	TimeStamp   uint64             `json:"timestamp"`
+	GasPrice    uint64             `json:"gas_price"`
+	GasUnits    uint64             `json:"gas_units"`
+	Sig         string             `json:"sig"`
+	Proof       []string           `json:"proof"`
+	ProofOrder  []int64            `json:"proof_order"`
+}
--- app/services/node/handlers/v1/public/public.go
+++ app/services/node/handlers/v1/public/public.go
@@ -3,8 +3,11 @@ package public
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 
+	"github.com/Kunmeer-SyedMohamedHyder/blockchain/business/sys/validate"
+	"github.com/Kunmeer-SyedMohamedHyder/blockchain/foundation/blockchain/database"
 	"github.com/Kunmeer-SyedMohamedHyder/blockchain/foundation/blockchain/state"
 	"github.com/Kunmeer-SyedMohamedHyder/blockchain/foundation/web"
 	"go.uber.org/zap"
@@ -16,9 +19,109 @@ type Handlers struct {
 	State *state.State
 }
 
+// SubmitWalletTransaction adds new transactions to the mempool.
+func (h Handlers) SubmitWalletTransaction(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+	v, err := web.GetValues(ctx)
+	if err != nil {
+		return web.NewShutdownError("web value missing from context")
+
+	}
+
+	// Decode the JSON in the post call into a Signed transaction.
+	var signedTx database.SignedTx
+	if err := web.Decode(r, &signedTx); err != nil {
+		return validate.NewRequestError(
+			fmt.Errorf("invalid payload to decode: %w", err), http.StatusBadRequest)
+	}
+
+	h.Log.Infow("add tran", "traceid", v.TraceID, "sig:nonce", signedTx, "from", signedTx.FromID, "to", signedTx.ToID, "value", signedTx.Value, "tip", signedTx.Tip)
+
+	// Ask the state package to add this transaction to the mempool. Only the
+	// checks are the transaction signature and the recipient account format.
+	// It's up to the wallet to make sure the account has a proper balance and
+	// nonce. Fees will be taken if this transaction is mined into a block.
+	if err := h.State.UpsertWalletTransaction(signedTx); err != nil {
+		return validate.NewRequestError(err, http.StatusBadRequest)
+	}
+
+	resp := struct {
+		Status string `json:"status"`
+	}{
+		Status: "transactions added to mempool",
+	}
+
+	return web.Respond(ctx, w, resp, http.StatusOK)
+}
+
 // Genesis returns the genesis information.
 func (h Handlers) Genesis(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
 	genesis := h.State.Genesis()
 
 	return web.Respond(ctx, w, genesis, http.StatusOK)
+}
+
+// Mempool returns the set of uncommitted transactions.
+func (h Handlers) Mempool(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+	acct := web.Param(r, "account")
+
+	mempool := h.State.Mempool()
+
+	trans := []tx{}
+	for _, tran := range mempool {
+		if acct != "" && ((acct != string(tran.FromID)) && (acct != string(tran.ToID))) {
+			continue
+		}
+
+		trans = append(trans, tx{
+			FromAccount: tran.FromID,
+			To:          tran.ToID,
+			ChainID:     tran.ChainID,
+			Nonce:       tran.Nonce,
+			Value:       tran.Value,
+			Tip:         tran.Tip,
+			Data:        tran.Data,
+			TimeStamp:   tran.TimeStamp,
+			GasPrice:    tran.GasPrice,
+			GasUnits:    tran.GasUnits,
+			Sig:         tran.SignatureString(),
+		})
+	}
+
+	return web.Respond(ctx, w, trans, http.StatusOK)
+}
+
+// Accounts returns the current balances for all users.
+func (h Handlers) Accounts(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+	accountStr := web.Param(r, "account")
+
+	var accounts = make(map[database.AccountID]database.Account)
+	switch accountStr {
+	case "":
+		accounts = h.State.Accounts()
+	default:
+		accountID, err := database.ToAccountID(accountStr)
+		if err != nil {
+			return err
+		}
+
+		account, err := h.State.QueryAccount(accountID)
+		if err != nil {
+			return err
+		}
+
+		accounts = map[database.AccountID]database.Account{accountID: account}
+	}
+
+	resp := make([]act, 0)
+	for accontID, accountInfo := range accounts {
+		act := act{
+			Account: accontID,
+			Balance: accountInfo.Balance,
+			Nonce:   accountInfo.Nonce,
+		}
+
+		resp = append(resp, act)
+	}
+
+	return web.Respond(ctx, w, resp, http.StatusOK)
 }
